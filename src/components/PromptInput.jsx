806f6bbdd03445dc107f1d3c9257e2a9aa72935f
Bakha022 <abduqahhorvbahodir536@gmail.com>
2024-12-24 21:36:56 +0500

    doneee

--- src/components/PromptInput.jsx
+++ src/components/PromptInput.jsx
@@ -6,12 +6,16 @@ const PromptInput = () => {
 	const textareaRef = useRef()
 
 	const handleInput = () => {
-		if (!textareaRef.current) return
-
 		const textarea = textareaRef.current
 
 		// Balandlikni qayta o'rnatish
 		textarea.style.height = 'auto'
+
+		// Yangi balandlikni o'lchash
+		const newHeight = textarea.scrollHeight
+
+		// Minimal balandlikni saqlash
+		textarea.style.height = `${Math.max(newHeight, 23)}px`
 	}
 
 	const {
@@ -19,25 +23,22 @@ const PromptInput = () => {
 		formState: { errors },
 		handleSubmit,
 	} = useForm()
-	const onSubmit = data => data
+	const onSubmit = data => console.log(data)
 
 	return (
 		<form
-			className={`input-section bg-[#F2F2F2] flex py-4 px-[26px] w-[708px] rounded-[20px] justify-between ${
-				errors?.text?.type == 'required' ? 'border-2 border-red-600' : ''
-			} `}
 			onSubmit={handleSubmit(onSubmit)}
+			className='input-section bg-[#F2F2F2] flex py-4 px-[26px] w-[75%] rounded-[20px] justify-between'
 		>
 			<textarea
 				rows={1}
 				ref={textareaRef}
 				onInput={handleInput}
 				placeholder='Пишите тут'
-				className={`bg-[#F2F2F2] w-[95%] h-auto resize-none outline-none`}
+				className='bg-[#F2F2F2] w-[95%] h-auto resize-none outline-none'
 				type='text'
-				{...register('text', { required: true })}
 			></textarea>
-			<button className='bg-none border-none'>
+			<button className='bg-none border-none flex items-end'>
 				<FaArrowCircleUp className='cursor-pointer' size={30} />
 			</button>
 		</form>
--- src/pages/protected/PromptPage.jsx
+++ src/pages/protected/PromptPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Title from "../../components/Title"
 import PrompInput from '../../components/PromptInput'
+import Title from '../../components/Title'
 const PromptPage = () => {
 	return (
 		<div className='container'>
 			<div className='flex flex-col justify-between gap-7 mt-[133px] xs:mt-[273px] items-center'>
 				<Title title={'Расскажите о себе'} />
-				<p className='font-montserrat font-medium text-base leading-5 w-[638px] text-center'>Чем вы занимаетесь, ваш опыт, сколько лет занимаетесь, где вы учились, достижения или если у вас нет опыта кем вы бы хотели работать. Чем подробно вы опишите тем круче будет результат</p>
+				<p className='font-montserrat font-medium text-base leading-5 text-center max-w-[638px]'>
+					Чем вы занимаетесь, ваш опыт, сколько лет занимаетесь, где вы учились,
+					достижения или если у вас нет опыта кем вы бы хотели работать. Чем
+					подробно вы опишите тем круче будет результат
+				</p>
 				<PrompInput />
 			</div>
 		</div>
