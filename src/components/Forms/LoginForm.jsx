4c36de4b2d6091df75b54a4c20d1478ae483a0b8
Bakha022 <abduqahhorvbahodir536@gmail.com>
2024-12-26 15:41:56 +0500

    validation added register and login form

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import Button from '../Button'
 
 const LoginForm = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm()
+	const onSubmit = data => console.log(data)
 	return (
-		<div className='flex flex-col gap-9  xs:gap-11 mt-[15px] xs:mt-[50px] items-center'>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className='flex flex-col gap-9  xs:gap-11 mt-[15px] xs:mt-[50px] items-center'
+		>
 			<input
 				type='text'
-				className='w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] border-[#00000066]'
+				className={`w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] placeholder-[#00000066]  border-[#00000066] ${
+					errors.userName ? 'placeholder-red-500 border-red-500' : ''
+				} `}
+				{...register('userName', { required: true })}
 				placeholder='Username'
 			/>
 			<input
 				type='password'
-				className='w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] border-[#00000066]'
+				className={`w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] placeholder-[#00000066]  border-[#00000066] ${
+					errors.password ? 'placeholder-red-500 border-red-500' : ''
+				} `}
+				{...register('password', { required: true })}
 				placeholder='Password'
 			/>
 			<Button word={'Войти'} />
-		</div>
+		</form>
 	)
 }
 
--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import Button from '../Button'
 
 const RegisterForm = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm()
+	const onSubmit = data => console.log(data)
+
 	return (
-		<div className='flex flex-col gap-9  xs:gap-11 mt-[15px] xs:mt-[50px] items-center'>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className='flex flex-col gap-9  xs:gap-11 mt-[15px] xs:mt-[50px] items-center'
+		>
 			<input
 				type='text'
-				className='w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] border-[#00000066]'
+				className={`w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] placeholder-[#00000066]  border-[#00000066] ${
+					errors.userName ? 'placeholder-red-500 border-red-500' : ''
+				} `}
+				{...register('userName', { required: true })}
 				placeholder='Username'
 			/>
 			<input
 				type='password'
-				className='w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] border-[#00000066]'
+				className={`w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] placeholder-[#00000066]  border-[#00000066] ${
+					errors.password ? 'placeholder-red-500 border-red-500' : ''
+				} `}
 				placeholder='Password'
+				{...register('password', { required: true })}
 			/>
 			<input
 				type='password'
-				className='w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] border-[#00000066]'
+				className={`w-full text-xl font-medium leading-[42px] montserrat text-center  xs:text-[28px] border-b-[3px] placeholder-[#00000066]  border-[#00000066] ${
+					errors.confirmPassword ? 'placeholder-red-500 border-red-500' : ''
+				} `}
 				placeholder='Confirm password'
+				{...register('confirmPassword', { required: true })}
 			/>
 			<Button word={'Регистрация'} />
-		</div>
+		</form>
 	)
 }
 
