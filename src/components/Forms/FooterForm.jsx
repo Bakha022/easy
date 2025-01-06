f34df3fc281535e7f410f744875ad9cc1f81cad3
Bakha022 <abduqahhorvbahodir536@gmail.com>
2025-01-06 16:38:04 +0500

    footer validation

--- src/components/Forms/FooterForm.jsx
+++ src/components/Forms/FooterForm.jsx
@@ -1,25 +1,51 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import Button from '../Button'
+
 const FooterForm = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm()
+	const onSubmit = data => console.log(data)
 	return (
-		<form className='w-[80%] md:w-[649px] shadow-custom rounded-[30px] mx-auto py-9 px-[70px] absolute bg-white top-[-400px] md:top-[-440px]'>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className='w-[80%] md:w-[649px] shadow-custom rounded-[30px] mx-auto py-9 px-[70px] absolute bg-white top-[-400px] md:top-[-440px]'
+		>
 			<h3 className='font-montserrat font-semibold text-2xl sm:text-3xl  md:text-4xl  mb-4  md:mb-[90px] leading-7 sm:leading-10 text-center'>
 				Связаться с нами
 			</h3>
 			<div className='flex flex-col gap-9 mb-[80px]'>
 				<input
+					{...register('name', { required: true })}
 					placeholder='Имя'
-					className='bg-none py-2 border-b border-black border-solid w-full  md:w-[165px] placeholder:text-black text-black'
-					type='text'
+					className={`bg-none py-2 border-b ${
+						errors.name
+							? 'border-red-500 placeholder:text-red-500 '
+							: 'border-black placeholder:text-black '
+					} border-solid w-full  md:w-[165px] text-black'
+					type='text`}
 				/>
 				<input
+					{...register('email', { required: true })}
 					placeholder='Почта'
-					className='bg-none py-2 border-b border-black border-solid placeholder:text-black text-black w-full  md:w-[250px]'
+					className={`bg-none py-2 border-b ${
+						errors.email
+							? 'border-red-500 placeholder:text-red-500 '
+							: 'border-black placeholder:text-black '
+					}  border-solid placeholder:text-black text-black w-full  md:w-[250px]`}
 					type='email'
 				/>
 				<input
+					{...register('message', { required: true })}
 					placeholder='Сообщение'
-					className='bg-none py-2 border-b border-black border-solid placeholder:text-black text-black w-full'
+					className={`bg-none py-2 border-b ${
+						errors.message
+							? 'border-red-500 placeholder:text-red-500 '
+							: 'border-black placeholder:text-black '
+					} border-solid placeholder:text-black text-black w-full `}
 					type='text'
 				/>
 			</div>
